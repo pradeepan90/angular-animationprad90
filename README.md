fc7f6d42ec33e34b2fab93d440905df0aaeea8ff
pradeepan90 <pradeepan90@gmail.com>
2020-05-06 12:06:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-animationprad90
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-animationprad90)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component'
+import {OpenCloseComponent} from "../open-close/open-close.component"
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    OpenCloseComponent
+  ],
+  declarations: [ ],
+  bootstrap: [AppComponent ]
+})
+export class AppModule { }
