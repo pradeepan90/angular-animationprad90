1cec2d8634e04363687a75484c2f5db912e50b35
pradeepan90 <pradeepan90@gmail.com>
2020-05-06 12:06:55 +0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component'
-import {OpenCloseComponent} from "../open-close/open-close.component"
+import {OpenCloseComponent} from "./open-close/open-close.component"
 
 @NgModule({
   imports: [
